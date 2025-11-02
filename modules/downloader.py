ba7c8dc68d342561c95b29fa53e5252188052445
jonas-kupferschmid <jonasku888@gmail.com>
2025-11-03 00:38:43 +0800

    deleted comments

--- modules/downloader.py
+++ modules/downloader.py
@@ -9,8 +9,6 @@ from typing import Optional
 
 import requests
 
-#comment
-#comment2
 from .config import CONFIG
 from .utils import atomic_write_json, make_tempfile
 from .scholar_client import RateLimiter
